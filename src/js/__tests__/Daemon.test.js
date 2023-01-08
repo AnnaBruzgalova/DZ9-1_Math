34fc6299a47be7b36f110451dd29546d8b0ccaa6
Anna Bryzgalova <annabryzgalova1984@gmail.com>
2023-01-08 18:15:24 +0300

    решение1

---
+++ src/js/__tests__/Daemon.test.js
@@ -0,0 +1,13 @@
+import Character from '../Daemon';
+
+test('create Character ok', () => {
+    const character = new Character('Anna', 'Daemon');
+    const result = {
+        name: 'Anna',
+        type: 'Daemon',
+        health: 100,
+        level: 1,
+        defence: 40,
+    };
+    expect(character).toMatchObject(result);
+});
